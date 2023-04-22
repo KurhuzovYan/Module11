2c101d693faa9bc6e10e91d6642113f317e22203
Yan Kurhuzov <yan.dasty@gmail.com>
2023-04-22 13:58:53 +0300

    Commit 3: Refactored code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
         StreamExample se = new StreamExample();
         List<String> list1 = Arrays.asList("Kolya", "Katya", "Zhenya", "Oleg", "Anya", "Sergey", "Pavel");
-        List<String> list2 = Arrays.asList("Pavel", "Mikhail", "Lina", "Alex", "Nika");
+        List<String> list2 = Arrays.asList("Pavel", "Mikhail", "Zack", "Lina", "Alex", "Nika");
 
-        se.getOddIndexOfNames(list1);
-        se.getSortedNamesInUpperCase(list2);
-        se.getSortedNumbers(new String[]{"1, 2, 0", "4, 5"});
+        System.out.println(se.getOddIndexOfNames(list1));
+        System.out.println(se.getSortedNumbers(new String[]{"1, 2, 0", "4, 5"}));
+        System.out.println(se.getSortedNumbers(new String[]{"0.6, 2.2, 0", "4.3, -5.2"}));
         //se.infinityStream(25214903917l, 11, (long) Math.pow(2, 48), 1).forEach(System.out::println);
         System.out.println(se.zip(list1.stream(), list2.stream()).collect(Collectors.joining(", ")));
     }
--- src/main/java/StreamExample.java
+++ src/main/java/StreamExample.java
@@ -1,45 +1,29 @@
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamExample {
-    public void getOddIndexOfNames(List<String> str) {
-        Map<Integer, String> namesMap = new HashMap<>();
-
-        for (int i = 0; i < str.size(); i++) {
-            namesMap.put(i, str.get(i));
-        }
-
-        Map<Integer, String> sortedNames = namesMap.entrySet().stream()
-                .filter(n -> n.getKey() % 2 == 1)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        String res = sortedNames.entrySet().stream()
-                .map(n -> n.getKey() + ". " + n.getValue())
+    public String getOddIndexOfNames(List<String> str) {
+        return IntStream.range(0, str.size())
+                .filter(i -> i % 2 == 1)
+                .mapToObj(i -> i + "." + str.get(i))
                 .collect(Collectors.joining(", "));
-
-        System.out.println(res);
     }
 
-    public void getSortedNamesInUpperCase(List<String> str) {
-        String res = str.stream()
+    public List<String> getSortedNamesInUpperCase(List<String> str) {
+        return str.stream()
                 .map(word -> word.toUpperCase())
                 .sorted(Comparator.reverseOrder())
-                .collect(Collectors.joining(", "));
-
-        System.out.println(res);
+                .collect(Collectors.toList());
     }
 
-    public void getSortedNumbers(String[] num) {
-        String collect = Arrays.stream(num)
-                .collect(Collectors.joining(", "));
-
-        String res = Arrays.stream(collect.split(" "))
+    public String getSortedNumbers(String[] num) {
+        return Arrays.stream(num)
+                .map(x -> x.replaceAll("\\s+", "").split(","))
+                .flatMap(x -> Arrays.stream(x))
                 .sorted()
-                .map(n -> n + " ")
-                .collect(Collectors.joining());
-
-        System.out.println(res);
+                .collect(Collectors.joining(", "));
     }
 
     public Stream<Long> infinityStream(long a, long c, long m, long seed) {
